82c20ee5eaaf7bb8a026963d83a1168548232892
Vijin Vinod <vfcvijin@gmail.com>
2023-03-16 00:02:50 +0530

    some bug in naming

--- util/multer.js
+++ util/multer.js
@@ -5,8 +5,11 @@ const storage = multer.diskStorage({
       destination: function (req, file, cb) {
         if (file.fieldname !== 'image') {
           cb(null, './public/admin/assets/img/products')
+          console.log("product uploaded");
         } else {
           cb(null, './public/admin/assets/img/products')
+          console.log("product uploaded");
+
         }
       },
       filename: function (req, file, cb) {
